7e6d93f7298db5632a27bb5caebed62d8cda5190
Katya Perlin <kaktuss@gmail.com>
2021-08-19 10:46:59 +0300

    Added components and assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PickerComponent } from './picker/picker.component';
+import { PreviewComponent } from './preview/preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PickerComponent,
+    PreviewComponent
   ],
   imports: [
     BrowserModule
